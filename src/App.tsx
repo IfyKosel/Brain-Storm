bbdd218d093752fe20da0fb88989c8a64c3ce2a4
Ifeoma <ifeoma.ofodile.231807@unn.edu.ng>
2024-01-27 10:49:21 +0100

    first commit: the Footer coponent

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import Footer from "./Components/Footer/footer";
+
+function App() {
+    return (
+        <>
+            <h1 className="capitalize text-red-300 bg-green-900 h-[20vh]">
+                dont play with me
+            </h1>
+            <Footer />
+        </>
+    );
+}
+
+export default App;
